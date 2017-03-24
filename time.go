51b5036de4969ddec0822ab7dbf10cc48a6e96f3
Brooke Bryan <brooke@bajb.net>
2017-03-24 10:50:13 +0000

    Time format as RFC3339Nano

--- time.go
+++ time.go
@@ -2,14 +2,12 @@ package intersights
 
 import "time"
 
-const timeFormat = "Mon Jan 2 15:04:05.000 -0700 MST 2006"
-
 type Time struct {
 	time.Time
 }
 
 func (j Time) format() string {
-	return j.Time.Format(timeFormat)
+	return j.Time.Format(time.RFC3339Nano)
 }
 
 func (j Time) MarshalText() ([]byte, error) {
